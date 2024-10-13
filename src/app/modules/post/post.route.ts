3948437a8157678d78c85e644caa054039178034
Anikroy7 <roy645800@gmail.com>
2024-10-13 19:22:49 +0800

    route change

--- src/app/modules/post/post.route.ts
+++ src/app/modules/post/post.route.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', /* auth(USER_ROLE.user, USER_ROLE.admin), */ PostControllers.getAllPosts)
 router.get('/my-posts', /* auth(USER_ROLE.user, USER_ROLE.admin), */ PostControllers.getMyPosts)
 router.post('/', /* auth(USER_ROLE.user), */  PostControllers.createPost)
-router.put('/:postId', auth(USER_ROLE.user, USER_ROLE.admin), PostControllers.updatePost)
+router.put('/:postId',/*  auth(USER_ROLE.user, USER_ROLE.admin), */ PostControllers.updatePost)
 router.get('/:postId', auth(USER_ROLE.user, USER_ROLE.admin), PostControllers.getPost)
 router.delete('/:postId', auth(USER_ROLE.user, USER_ROLE.admin), PostControllers.deletePost)
 
